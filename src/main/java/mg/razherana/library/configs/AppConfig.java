0e7d028c9bfe31c403b77329f43979fa9d2590bb
razherana <razherana@gmail.com>
2025-07-01 12:52:30 +0300

    Init commit 2

--- src/main/java/mg/razherana/library/configs/AppConfig.java
+++ src/main/java/mg/razherana/library/configs/AppConfig.java
@@ -2,6 +2,7 @@ package mg.razherana.library.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -11,7 +12,7 @@ import org.thymeleaf.templatemode.TemplateMode;
 public class AppConfig implements WebMvcConfigurer {
 
   @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+  public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/static/**")
         .addResourceLocations("classpath:/static/");
   }
